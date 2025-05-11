2152edb31a4a65e743bb2d21ea90830d30cf6bb4
sotisz <sotisz5305@naver.com>
2025-05-11 13:45:33 +0900

    TMP설치, Intro 씬

---
+++ Assets/Scripts/SceneMove.cs
@@ -0,0 +1,21 @@
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    public void MoveToGameScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnExitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# CapStone
-Unity Project : 웅장하고 비범하게
