e1747043f4b3b935154c4e112471ad9ef5cd5c36
sotisz <sotisz5305@naver.com>
2025-05-11 14:54:29 +0900

    TMP 설치, Intro 씬 구현
    Text Mesh Pro

--- Assets/script/SceneMove.cs
+++ Assets/script/SceneMove.cs
@@ -1,9 +1,13 @@
 using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneMove : MonoBehaviour
 {
+
+    public GameObject SettingPanel;
+
     public void MoveToGameScene()
     {
         SceneManager.LoadScene("SampleScene");
@@ -18,4 +22,12 @@ public class SceneMove : MonoBehaviour
 #endif
     }
 
+    public void OnSettingIn()
+    {
+        SettingPanel.SetActive(true);
+    }
+    public void OnSettingExit()
+    {
+        SettingPanel.SetActive(false);
+    }
 }
