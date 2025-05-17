d427560a3793884cdfb4761f7b6f691faafd0ab5
sotisz <sotisz5305@naver.com>
2025-05-17 13:26:26 +0900

    구데이터 삭제

--- Assets/Scripts/Tagbar.cs
+++ Assets/Scripts/Tagbar.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Tagebar : MonoBehaviour
+public class Tagbar : MonoBehaviour
 {
     public Image gaugeBarFill;
     public float gaugeBarFillCooldown = 0f;
