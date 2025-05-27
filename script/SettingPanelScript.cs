b06eab9a9585d59324dd6c1d3ed3f8fb18df8d11
sotisz <157656649+sotisz@users.noreply.github.com>
2025-05-27 11:18:48 +0900

    오류 수정

---
+++ script/SettingPanelScript.cs
@@ -0,0 +1,47 @@
+
+
+
+// 환경설정 창 prefab >> 하이어라키 창에 올려두고,
+// 띄우는 키 혹은 버튼만 지정하면 어디서든 불러오기 가능!!
+
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+
+
+// 나중에 음량, 키지정, 해상도 조작 필요
+public class SettingPanelScript : MonoBehaviour
+{
+
+    public GameObject SettingPanel;
+
+    
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            OnSettingExit();
+        }
+    } 
+
+    
+    public void Mute()
+    {
+        SoundManager.Instance.SetAudioMute(EAudioMixerType.BGM);
+    }
+    public void ChangeVolume(float volume)
+    {
+        SoundManager.Instance.SetAudioVolume(EAudioMixerType.BGM, volume);
+    }
+    public void OnSettingIn()
+    {
+        SettingPanel.SetActive(true);
+    }
+    public void OnSettingExit()
+    {
+        SettingPanel.SetActive(false);
+    }
+}
