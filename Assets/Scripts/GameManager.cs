1f02653f2217feb7589750771e2ef2feece956f6
jaehwanl705 <63949259+jaehwanl705@users.noreply.github.com>
2025-07-18 15:56:04 +0900

    20250718 대화창 ui 예시안 대화창 스크립트 구상중

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameState == "paused")
+        {
+            
+        }
     }
 }
