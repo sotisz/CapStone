315d7eab31f1e745559b172d81666fbb225d379f
sotisz <sotisz5305@naver.com>
2025-12-11 22:08:05 +0900

    4스테이지 완성~

--- Assets/script/PausePanelScript.cs
+++ Assets/script/PausePanelScript.cs
@@ -5,6 +5,7 @@ public class PausePanelScript : MonoBehaviour
 {
     public GameObject PausePanel;
     public GameObject SettingPanel;
+    public GameObject ClearPanel;
 
     // 테스트용 업데이트 메소드 << 나중에 게임매니저 스크립트로 옮기기
     // (게임을 멈춰야 하기 때문에)
@@ -12,6 +13,10 @@ public class PausePanelScript : MonoBehaviour
     {
         if (Input.GetButtonDown("Cancel"))
         {
+            if (ClearPanel.activeSelf)
+            {
+                return;
+            }
             if (PausePanel.activeSelf) // 일시정지상태에서
             {
                 if (SettingPanel.activeSelf) // 환경설정이 켜져있으면
